784eae3575c9c9afb5a1840f08cbc972c3e40c2c
rtnaapuspitaa-bit <rtnaapuspitaa@gmail.com>
2026-09-02 12:10:21 +0700

    Ganti link linkedin

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
     {
       title: 'LinkedIn',
       value: 'Ratna Puspita Sari',
-      link: 'https://www.linkedin.com/in/ratna-puspita-sari-5836b1428/details/education/',
+      link: 'https://www.linkedin.com/in/ratna-puspita-sari-5836b1428/',
       icon: (
         <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24">
           <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.28 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.75M6.46 10.9v8.37H9.25V10.9H6.46M7.86 6.69a1.63 1.63 0 1 0 0 3.26 1.63 1.63 0 0 0 0-3.26Z"/>

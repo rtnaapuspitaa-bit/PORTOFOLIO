d357efb9cd2b00d368804f4bf772d2dd71fecd0d
rtnaapuspitaa-bit <rtnaapuspitaa@gmail.com>
2026-09-02 13:36:26 +0700

    link email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
     {
       title: 'Email',
       value: 'rtnaapuspitaa@gmail.com',
-      link: 'mailto:rtnaapuspitaa@gmail.com',
+      link: 'https://mail.google.com/mail/?view=cm&fs=1&to=rtnaapuspitaa@gmail.com',
       icon: (
         <svg className="w-8 h-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
